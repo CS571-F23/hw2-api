342317f243639ade5df7e46739fd6311fa6e96f2
Cole Nelson <ctnelson1997@gmail.com>
2023-09-19 09:42:11 -0500

    add xss route

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import express, { Express } from 'express';
 import { CS571DefaultSecretConfig, CS571Initializer } from '@cs571/f23-api-middleware'
 import { CS571StudentsRoute } from './routes/students';
 import HW2PublicConfig from './model/configs/hw2-public-config';
+import { CS571StudentsXSSRoute } from './routes/students-xss';
 
 console.log("Welcome to HW2!");
 
@@ -16,9 +17,12 @@ const appBundle = CS571Initializer.init<HW2PublicConfig, CS571DefaultSecretConfi
 });
 
 const students = JSON.parse(fs.readFileSync(appBundle.config.PUBLIC_CONFIG.STUDENT_INFO_LOC).toString())
+const studentsXss = JSON.parse(fs.readFileSync("includes/students.xss").toString())
+
 
 appBundle.router.addRoutes([
-  new CS571StudentsRoute(students)
+  new CS571StudentsRoute(students),
+  new CS571StudentsXSSRoute(studentsXss)
 ])
 
 app.listen(appBundle.config.PORT, () => {
